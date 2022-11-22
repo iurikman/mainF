81f88967576cce9ee40a62e9cc9d2be8eb509bcf
Iurikman <y.m.kutyrev@gmail.com>
2022-11-22 23:53:00 +0300

    4 commit

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+void Mmoi(int c, int d) {
+    int a = c;
+    int b = d;
+    Console.WriteLine(a*b);
+}
+
+Mmoi(2,4);
